0bf11cdd84357d615a74faadd15344188d381bbc
Samuel Défago <defagos@gmail.com>
2018-07-17 09:05:34 +0200

    Add demo project

---
+++ Demo/Sources/Demos/DemosViewController.m
@@ -0,0 +1,23 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import "DemosViewController.h"
+
+@interface DemosViewController ()
+
+@end
+
+@implementation DemosViewController
+
+#pragma mark Object lifecycle
+
+- (instancetype)init
+{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:NSStringFromClass(self.class) bundle:nil];
+    return [storyboard instantiateInitialViewController];
+}
+
+@end
