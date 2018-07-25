399e810ea371a3af4045fa5645deeb482b0d8ac3
Samuel Défago <defagos@gmail.com>
2018-07-25 14:40:53 +0200

    Improve documentation

--- Framework/Sources/SRGAkamaiTokenService.h
+++ Framework/Sources/SRGAkamaiTokenService.h
@@ -19,10 +19,13 @@ NS_ASSUME_NONNULL_BEGIN
 + (instancetype)sharedService;
 
 /**
- *  Return a network request for retrieving an Akamai token for the specified URL.
+ *  Return a network request for retrieving an Akamai token for the specified URL. Except in special cases where
+ *  you need to provide the URL to another context (e.g. Google Cast), there is in general no need for explicit
+ *  tokenization in client code. When preparing an item for `AVPlayer` playback, build your asset using the
+ *  `+[AVURLAsset srg_assetWithURL:contentProtection:]` method instead.
  *
- *  @discussion If the request fails, the original URL is returned and can be used to attempt playback (without
- *              guarantee that this will succeed, though).
+ *  @discussion If the request fails, the original URL is returned in the completion block and can be used to attempt
+ *              playback (without guarantee that this will succeed, though).
  */
 - (SRGNetworkRequest *)tokenizeURL:(NSURL *)URL withCompletionBlock:(void (^)(NSURL *URL))completionBlock;
 
