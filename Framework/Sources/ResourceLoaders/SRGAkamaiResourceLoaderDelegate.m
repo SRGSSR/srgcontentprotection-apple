61caa84bd35720bf64a1416b6b2c7e48ca8dc089
Samuel Défago <defagos@gmail.com>
2018-07-25 18:17:39 +0200

    Retrieve playlist data

--- Framework/Sources/ResourceLoaders/SRGAkamaiResourceLoaderDelegate.m
+++ Framework/Sources/ResourceLoaders/SRGAkamaiResourceLoaderDelegate.m
@@ -12,6 +12,7 @@
 
 @interface SRGAkamaiResourceLoaderDelegate ()
 
+@property (nonatomic) NSURLSession *session;
 @property (nonatomic) SRGNetworkRequest *request;
 
 @end
@@ -43,6 +44,16 @@
     return components.URL;
 }
 
+#pragma mark Object lifecycle
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        self.session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    }
+    return self;
+}
+
 #pragma mark Common resource loading request processing
 
 - (BOOL)shouldProcessResourceLoadingRequest:(AVAssetResourceLoadingRequest *)loadingRequest
@@ -50,15 +61,25 @@
     dispatch_async(dispatch_get_main_queue(), ^{
         NSURL *URL = [SRGAkamaiResourceLoaderDelegate URLForAssetURL:loadingRequest.request.URL];
         self.request = [[SRGAkamaiTokenService sharedService] tokenizeURL:URL withCompletionBlock:^(NSURL *tokenizedURL) {
-            NSMutableURLRequest *redirect = [loadingRequest.request mutableCopy];
-            redirect.URL = tokenizedURL;
-            loadingRequest.redirect = [redirect copy];
-            
-            // Force redirect to the new tokenized URL
-            NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:tokenizedURL statusCode:303 HTTPVersion:nil headerFields:nil];
-            [loadingRequest setResponse:response];
-            
-            [loadingRequest finishLoading];
+            NSURLRequest *playlistRequest = [NSURLRequest requestWithURL:tokenizedURL];
+            self.request = [[SRGNetworkRequest alloc] initWithURLRequest:playlistRequest session:self.session options:0 completionBlock:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (error) {
+                        NSMutableDictionary *userInfo = [@{ NSLocalizedDescriptionKey : SRGContentProtectionLocalizedString(@"This stream is protected and cannot be read without proper rights.", @"User-facing message displayed when an error related to digital rights management (DRM) has been encountered") } mutableCopy];
+                        if (error) {
+                            userInfo[NSUnderlyingErrorKey] = error;
+                        }
+                        NSError *friendlyError = [NSError errorWithDomain:SRGContentProtectionErrorDomain code:SRGContentProtectionErrorUnauthorized userInfo:[userInfo copy]];
+                        [loadingRequest finishLoadingWithError:friendlyError];
+                        return;
+                    }
+                    else {
+                        [loadingRequest.dataRequest respondWithData:data];
+                        [loadingRequest finishLoading];
+                    }
+                });
+            }];
+            [self.request resume];
         }];
         [self.request resume];
     });
